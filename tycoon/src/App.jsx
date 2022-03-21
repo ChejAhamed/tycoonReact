caef701464b74566ef89b99a770a163674b88e5f
Chej Ahamed <74456334+ChejAhamed@users.noreply.github.com>
2022-03-21 14:36:30 +0000

    Add navation

--- tycoon/src/App.jsx
+++ tycoon/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './compoments/Header';
+import Footer from './compoments/Footer';
+
+import Home from './pages/Home';
+import Tycoon from './pages/Tycoon';
+import Aboutus from './pages/Aboutus';
 
 function App() {
   return (
 
-    <p>dddd</p>
+    <Router>
+      <Header />
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/tycoon" element={<Tycoon />} />
+
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- tycoon/src/compoments/Header.jsx
+++ tycoon/src/compoments/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>Header</div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/tycoon">Tycoon</Link>
+      <Link to="/aboutus">Aboutus</Link>
+    </nav>
   );
 }
 
