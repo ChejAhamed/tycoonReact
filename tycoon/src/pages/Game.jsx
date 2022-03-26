89c23c5c58da7f89d582009bce798156b939f312
Chej Ahamed <74456334+ChejAhamed@users.noreply.github.com>
2022-03-26 13:13:58 +0000

    Add button to Game page

--- tycoon/src/pages/Game.jsx
+++ tycoon/src/pages/Game.jsx
@@ -9,6 +9,26 @@ function Game() {
         Game
       </div>
       WELLCOME TO THE GAME
+      <div className="menu">
+        <div className="humanPlayers">
+          <button type="button" className="topHat  btngame">Top Hat</button>
+          <button type="button" className="ship  btngame">Ship</button>
+          <button type="button" className="dog  btngame">Dog</button>
+          <button type="button" className="shoe  btngame">Shoe</button>
+          <button type="button" className="car  btngame">Car</button>
+          <button type="button" className="bird  btngame">bird</button>
+
+        </div>
+        <div className="aiPlayers">
+          <button type="button" className="topHat  btngame">Top Hat</button>
+          <button type="button" className="ship  btngame">Ship</button>
+          <button type="button" className="dog  btngame">Dog</button>
+          <button type="button" className="shoe  btngame">Shoe</button>
+          <button type="button" className="car  btngame">Car</button>
+          <button type="button" className="bird  btngame">bird</button>
+
+        </div>
+      </div>
       <div className="board" />
     </div>
   );
