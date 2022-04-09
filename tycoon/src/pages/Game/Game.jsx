0b77da592cb18b8f37795fa272d596193a17b68f
Chej Ahamed <74456334+ChejAhamed@users.noreply.github.com>
2022-04-09 12:34:24 +0100

    Add ternary logic to game page

--- tycoon/src/pages/Game/Game.jsx
+++ tycoon/src/pages/Game/Game.jsx
@@ -279,6 +279,7 @@ function Game() {
       } else {
       // players[playerNumber] = players[playerNumber];
         console.log(playerNumber);
+        console.log(theBoard[players[playerNumber].currentSpace]);
         theBoard[players[playerNumber].currentSpace] = theBoard[players[playerNumber].currentSpace];
       }
     } else {
@@ -487,106 +488,135 @@ function Game() {
   }
   movePlayer();
 */
+  const [showGame, setShowGame] = React.useState(false);
   return (
     // usar [...aarray, nuevoItem] en bottom para pushear
     <div className="game">
-      <div className="title">
+      {showGame ? (
+        <div className="secondWindow">
+          <div className="boardGameSec">
+            <div className="dice">
+              <button type="button" className="diceBnt" onClick={() => throwDice()}>Throw Dice</button>
+            </div>
+            <div id="board" className="board">
+              <div className="top">
+                <div id="20" className="parking corner" />
+                <div id="21" className="horizontal" />
+                <div id="22" className="horizontal" />
+                <div id="23" className="horizontal" />
+                <div id="24" className="horizontal" />
+                <div id="25" className="horizontal" />
+                <div id="26" className="horizontal" />
+                <div id="27" className="horizontal player1" />
+                <div id="28" className="horizontal" />
+                <div id="29" className="horizontal" />
+                <div id="0" className="jail corner" />
+              </div>
+              <div className="mid">
+                <div className="mid-left">
+                  <div id="19" className="vertical " />
+                  <div id="18" className="vertical" />
+                  <div id="17" className="vertical" />
+                  <div id="16" className="vertical" />
+                  <div id="15" className="vertical" />
+                  <div id="14" className="vertical" />
+                  <div id="13" className="vertical" />
+                  <div id="12" className="vertical" />
+                  <div id="11" className="vertical" />
+                </div>
+                <div className="mid-mid" />
+                <div className="mid-right">
+                  <div id="30" className="vertical" />
+                  <div id="31" className="vertical" />
+                  <div id="32" className="vertical" />
+                  <div id="33" className="vertical" />
+                  <div id="34" className="vertical" />
+                  <div id="35" className="vertical" />
+                  <div id="36" className="vertical" />
+                  <div id="37" className="vertical" />
+                  <div id="39" className="vertical" />
+                </div>
+              </div>
+              <div className="bottom">
+                <div id="0" className="jail corner" />
+                <div id="10" className="horizontal" />
+                <div id="9" className="horizontal" />
+                <div id="8" className="horizontal" />
+                <div id="7" className="horizontal" />
+                <div id="6" className="horizontal" />
+                <div id="5" className="horizontal" />
+                <div id="4" className="horizontal" />
+                <div id="3" className="horizontal" />
+                <div id="2" className="horizontal" />
+                <div id="1" className="collect corner" />
+              </div>
 
-        Game
-      </div>
-      WELLCOME TO THE GAME
-      <div className="menu">
-        <div className="humanPlayers">
-          <button type="button" disabled={disableButtonTopHat} onClick={() => { playerPlayingGame.push(players.human[0]); setDisableButtonTopHat(false); }} className="topHat  btngame">Top Hat</button>
-          <button type="button" disabled={disableButtonShip} onClick={() => { playerPlayingGame.push(players.human[1]); setDisableButtonShip(false); }} className="ship  btngame">Ship</button>
-          <button type="button" disabled={disableButtonDog} onClick={() => { playerPlayingGame.push(players.human[2]); setDisableButtonDog(false); }} className="dog  btngame">Dog</button>
-          <button type="button" disabled={disableButtonShoe} onClick={() => { playerPlaying.push(players.human[3]); setDisableButtonShoe(true); }} className="shoe  btngame">Shoe</button>
-          <button type="button" disabled={disableButtonCar} onClick={() => { playerPlaying.push(players.human[4]); setDisableButtonCar(true); }} className="car  btngame">Car</button>
-          <button type="button" disabled={disableButtonbird} onClick={() => { playerPlaying.push(players.human[5]); setDisableButtonBird(true); }} className="bird  btngame">Bird</button>
-
-        </div>
-        <div className="aiPlayers">
-          <button type="button" disabled={disableButtonTopHat} onClick={() => { playerPlaying.push(players.ai[0]); setDisableButtonTopHat(true); }} className="topHat  btngame">Top Hat</button>
-          <button type="button" disabled={disableButtonShip} onClick={() => { playerPlaying.push(players.ai[1]); setDisableButtonShip(true); }} className="ship  btngame">Ship</button>
-          <button type="button" disabled={disableButtonDog} onClick={() => { playerPlaying.push(players.ai[2]); setDisableButtonDog(true); }} className="dog  btngame">Dog</button>
-          <button type="button" disabled={disableButtonShoe} onClick={() => { playerPlaying.push(players.ai[3]); setDisableButtonShoe(true); }} className="shoe  btngame">Shoe</button>
-          <button type="button" disabled={disableButtonCar} onClick={() => { playerPlaying.push(players.ai[4]); setDisableButtonCar(true); }} className="car  btngame">Car</button>
-          <button type="button" disabled={disableButtonbird} onClick={() => { playerPlaying.push(players.ai[5]); setDisableButtonBird(true); }} className="bird  btngame">Bird</button>
-
-        </div>
-      </div>
-      <div className="gridBard" />
-      <div className="starGame">
-        <button type="button" className="startGameBnt" onClick={() => startGame()}>Start Game</button>
-      </div>
-      <div className="rules">
-        <div className="classic">
-          <button type="button" className="classicBnt" onClick={() => startGame()}>Classic</button>
-        </div>
-        <div className="quick">
-          <button type="button" className="quickBnt" onClick={() => startGame()}>Quick</button>
-        </div>
-        <div className="custom">
-          <button type="button" className="customBnt" onClick={() => startGame()}>Custom</button>
-        </div>
-      </div>
-      <div className="dice">
-        <button type="button" className="diceBnt" onClick={() => throwDice()}>Throw Dice</button>
-      </div>
-      <div id="board" className="board">
-        <div className="top">
-          <div id="20" className="parking corner" />
-          <div id="21" className="horizontal" />
-          <div id="22" className="horizontal" />
-          <div id="23" className="horizontal" />
-          <div id="24" className="horizontal" />
-          <div id="25" className="horizontal" />
-          <div id="26" className="horizontal" />
-          <div id="27" className="horizontal player1" />
-          <div id="28" className="horizontal" />
-          <div id="29" className="horizontal" />
-          <div id="0" className="jail corner" />
-        </div>
-        <div className="mid">
-          <div className="mid-left">
-            <div id="19" className="vertical " />
-            <div id="18" className="vertical" />
-            <div id="17" className="vertical" />
-            <div id="16" className="vertical" />
-            <div id="15" className="vertical" />
-            <div id="14" className="vertical" />
-            <div id="13" className="vertical" />
-            <div id="12" className="vertical" />
-            <div id="11" className="vertical" />
+            </div>
+            <div className="dialogPlayer">
+              <div className="dialogInfo">
+                info
+              </div>
+              <div className="dialogAction">
+                buttons
+              </div>
+            </div>
           </div>
-          <div className="mid-mid" />
-          <div className="mid-right">
-            <div id="30" className="vertical" />
-            <div id="31" className="vertical" />
-            <div id="32" className="vertical" />
-            <div id="33" className="vertical" />
-            <div id="34" className="vertical" />
-            <div id="35" className="vertical" />
-            <div id="36" className="vertical" />
-            <div id="37" className="vertical" />
-            <div id="39" className="vertical" />
+          <div className="infoWindow">
+            <div className="playerList">
+              player list
+            </div>
+            <div className="playerDetails">
+              details palyer
+            </div>
+
           </div>
         </div>
-        <div className="bottom">
-          <div id="0" className="jail corner" />
-          <div id="10" className="horizontal" />
-          <div id="9" className="horizontal" />
-          <div id="8" className="horizontal" />
-          <div id="7" className="horizontal" />
-          <div id="6" className="horizontal" />
-          <div id="5" className="horizontal" />
-          <div id="4" className="horizontal" />
-          <div id="3" className="horizontal" />
-          <div id="2" className="horizontal" />
-          <div id="1" className="collect corner" />
+
+      ) : (
+        <div className="firstWindow">
+          <div className="title">
+
+            Game
+          </div>
+          WELLCOME TO THE GAME
+          <div className="menu">
+            <div className="humanPlayers">
+              <button type="button" disabled={disableButtonTopHat} onClick={() => { playerPlayingGame.push(players.human[0]); setDisableButtonTopHat(false); }} className="topHat  btngame">Top Hat</button>
+              <button type="button" disabled={disableButtonShip} onClick={() => { playerPlayingGame.push(players.human[1]); setDisableButtonShip(false); }} className="ship  btngame">Ship</button>
+              <button type="button" disabled={disableButtonDog} onClick={() => { playerPlayingGame.push(players.human[2]); setDisableButtonDog(false); }} className="dog  btngame">Dog</button>
+              <button type="button" disabled={disableButtonShoe} onClick={() => { playerPlaying.push(players.human[3]); setDisableButtonShoe(true); }} className="shoe  btngame">Shoe</button>
+              <button type="button" disabled={disableButtonCar} onClick={() => { playerPlaying.push(players.human[4]); setDisableButtonCar(true); }} className="car  btngame">Car</button>
+              <button type="button" disabled={disableButtonbird} onClick={() => { playerPlaying.push(players.human[5]); setDisableButtonBird(true); }} className="bird  btngame">Bird</button>
+
+            </div>
+            <div className="aiPlayers">
+              <button type="button" disabled={disableButtonTopHat} onClick={() => { playerPlaying.push(players.ai[0]); setDisableButtonTopHat(true); }} className="topHat  btngame">Top Hat</button>
+              <button type="button" disabled={disableButtonShip} onClick={() => { playerPlaying.push(players.ai[1]); setDisableButtonShip(true); }} className="ship  btngame">Ship</button>
+              <button type="button" disabled={disableButtonDog} onClick={() => { playerPlaying.push(players.ai[2]); setDisableButtonDog(true); }} className="dog  btngame">Dog</button>
+              <button type="button" disabled={disableButtonShoe} onClick={() => { playerPlaying.push(players.ai[3]); setDisableButtonShoe(true); }} className="shoe  btngame">Shoe</button>
+              <button type="button" disabled={disableButtonCar} onClick={() => { playerPlaying.push(players.ai[4]); setDisableButtonCar(true); }} className="car  btngame">Car</button>
+              <button type="button" disabled={disableButtonbird} onClick={() => { playerPlaying.push(players.ai[5]); setDisableButtonBird(true); }} className="bird  btngame">Bird</button>
+
+            </div>
+          </div>
+          <div className="gridBard" />
+          <div className="rules">
+            <div className="classic">
+              <button type="button" className="classicBnt" onClick={() => startGame()}>Classic</button>
+            </div>
+            <div className="quick">
+              <button type="button" className="quickBnt" onClick={() => startGame()}>Quick</button>
+            </div>
+            <div className="custom">
+              <button type="button" className="customBnt" onClick={() => startGame()}>Custom</button>
+            </div>
+          </div>
+          <div className="starGame">
+            <button type="button" className="startGameBnt" onClick={() => { startGame(); setShowGame(true); }}>Start Game</button>
+          </div>
         </div>
 
-      </div>
+      )}
     </div>
   );
 }
