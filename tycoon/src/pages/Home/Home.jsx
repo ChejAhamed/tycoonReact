5db0ae475598cd58972d4e944d3df8cddccf34cd
Chej Ahamed <74456334+ChejAhamed@users.noreply.github.com>
2022-04-27 00:07:30 +0100

    Add styles to home page

--- tycoon/src/pages/Home/Home.jsx
+++ tycoon/src/pages/Home/Home.jsx
@@ -5,17 +5,31 @@ function Home() {
   return (
     <div className="home">
       <div className="title">Home</div>
-      <div className="container-home">
-        <dvi className="container-one">
+      <div className="container">
+        <dvi className="container-top">
           <div className="sussexPicture" />
-          Welcome to Tycoon Game. Made by students from Sussex University.
-
         </dvi>
-        <div className="container-two">
-          bb
+        <div className="container-bottom">
+          Welcome to Property Tycoon!
+
+          This game is made by 2nd year students from the University of Sussex!
+
+          <h4 className="h4h">About The Game:</h4>
+          Property Tycoon is Watson Games most popular title!
+          It is a property trading game similar to the classic Monopoly.
+          Players are able to roll the dice to move across the board
+          to purchase properties to collect rent from other players.
+
+          <h4 className="h4h"> How To Play</h4>
+          Select your desired character. Then you are able to roll your dice to move across
+          the board to purchase properties of your choice. These properties would allow you
+          to collect rent from the other players. There are also two packs of cards available
+          in the game - pot luck and opportunity knocks. They could either benefit the player
+          or knock progress from the player. The players who are unable to pay their rent
+          would exit the game through bankruptcy. The winner would remain, having the most
+          game assets.
         </div>
       </div>
-
     </div>
   );
 }
